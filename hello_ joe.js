205ee01754edffa4b5032f94683fe9a3a4aa92c5
mshava <sakhile@projectcodex.co>
2015-03-03 16:38:57 +0200

    functions 1

---
+++ hello_ joe.js
@@ -0,0 +1,11 @@
+
+function hello_joe(name){ 
+
+var username=prompt("enter your name"){
+
+if(username=== "joe" || username==="bob")
+	return ("hello");
+}else{
+	return("hello, " + username);
+}
+
---
+++ hello_joe_test.js~
@@ -0,0 +1,4 @@
+QUnit.test( "hello_joe", function( assert ) {
+var value = hello_joe("joe");
+assert.equal( value, "hello", "hello_joe" );
+});
