d9138c5ea7a0d036d504d145c3b4d828df75bf07
mshava <sakhile@projectcodex.co>
2015-11-02 17:07:17 +0200

    modified TDD.html,two new files reverse.js and the test

---
+++ reverse.js
@@ -0,0 +1,12 @@
+var reverse = function(codex academy){
+
+	var str = '';
+
+		for (var i = codex academy.length -1 ; i >= 0 ; i--)	
+
+			str += codex academy[i];
+
+	return str;
+
+}
+
---
+++ reverse_test.js
@@ -0,0 +1,5 @@
+QUnit.test("testing reverse string",function (assert){
+
+	assert.equal(split("codex academy"),"ymedaca xedoc","we are expecting ymedaca xedoc");
+
+});
